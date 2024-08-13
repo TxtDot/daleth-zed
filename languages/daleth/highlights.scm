e0749cc2635e98faf641ca8f9930fafac6d7d13e
Artemy Egorov <artegoser@gmail.com>
2024-08-13 21:03:16 +0300

    feat: tree-sitter support

---
+++ languages/daleth/highlights.scm
@@ -0,0 +1,21 @@
+(comment) @comment
+[
+  (number_argument)
+  (tag)
+] @tag
+
+
+(mlms_number) @number
+
+(textual) @string.special
+(escape) @string.escape
+(text_argument) @string
+
+(table_parser_id) @keyword
+
+[
+  (symbol_open)
+  (symbol_close)
+] @punctuation.bracket
+
+(text_body_open) @punctuation.delimiter
